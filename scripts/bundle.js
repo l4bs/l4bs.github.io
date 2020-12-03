c01a1e156440e030edb078714821ed2a2eeccf45
Renato Fabbri <renato.fabbri@gmail.com>
2020-12-03 11:40:01 -0300

    up

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -77399,6 +77399,7 @@ console.log('yeah man')
 window.wand = {
   router: require('./modules/router.js'),
   net: require('./modules/net.js'),
+  med: require('./modules/med'),
   test: require('./modules/test.js')
 }
 
@@ -77407,9 +77408,14 @@ if (page !== null) wand.test[wand.router.urlArgument('p')]()
 
 const meditation = wand.router.urlArgument('m') // identificator created in the dedicated page
 // if (meditation !== null) wand.test.atry(meditation)
-if (meditation !== null) wand.test.atry2(meditation)
+if (meditation !== null) {
+  wand.test.atry2(meditation)
+}
 
-},{"./modules/net.js":221,"./modules/router.js":222,"./modules/test.js":223}],217:[function(require,module,exports){
+const meditation2 = wand.router.urlArgument('mm')
+if (meditation2 !== null) wand.med.model3(meditation2)
+
+},{"./modules/med":218,"./modules/net.js":222,"./modules/router.js":223,"./modules/test.js":224}],217:[function(require,module,exports){
 const d = require('./utils.js').defaultArg
 const t = require('tone')
 
@@ -77424,13 +77430,14 @@ e.mkOsc = (freq, vol, pan, type, noConnect) => {
   return synth
 }
 
-},{"./utils.js":225,"tone":212}],218:[function(require,module,exports){
+},{"./utils.js":226,"tone":212}],218:[function(require,module,exports){
 module.exports = {
   model1: require('./model1').meditation,
-  model2: require('./model2').meditation
+  model2: require('./model2').meditation,
+  model3: require('./model3').meditation
 }
 
-},{"./model1":219,"./model2":220}],219:[function(require,module,exports){
+},{"./model1":219,"./model2":220,"./model3":221}],219:[function(require,module,exports){
 const t = require('tone')
 const $ = require('jquery')
 const PIXI = require('pixi.js')
@@ -77688,7 +77695,7 @@ e.meditation = mid => {
   const m2 = $('<div/>', { id: 'meter2' }).appendTo(grid)
 }
 
-},{"../maestro.js":217,"../transfer.js":224,"../utils.js":225,"jquery":60,"pixi.js":204,"tone":212}],220:[function(require,module,exports){
+},{"../maestro.js":217,"../transfer.js":225,"../utils.js":226,"jquery":60,"pixi.js":204,"tone":212}],220:[function(require,module,exports){
 const t = require('tone')
 const $ = require('jquery')
 const PIXI = require('pixi.js')
@@ -77715,6 +77722,7 @@ e.meditation = mid => {
   //    breath with the vertical position of the circles and the expansion of the circle to the (right, left, center, check bPos index)
   //  if clicked, show the inhale / exhale text
   // waveforms for LR
+  // info on the settings
   transfer.findAny({ meditation: mid }).then(s => { // s === settings
     if (s === null) {
       grid.css('background', 'red')
@@ -77756,6 +77764,7 @@ e.meditation = mid => {
       synth.volume.rampTo(-400, 10)
       synthR.volume.rampTo(-400, 10)
     }
+    grid.css('background', 'yellow')
   })
   function setCountdown (duration, fun, args) { // duration in seconds
     const targetTime = (new Date()).getTime() / 1000 + duration
@@ -77944,7 +77953,7 @@ e.meditation = mid => {
   const grid = utils.mkGrid(2)
   $('<div/>').appendTo(grid).text('status:')
   const countdown = $('<div/>', { id: 'countdown' }).appendTo(grid)
-  grid.css('background', 'yellow')
+  grid.css('background', 'grey')
 
   const vonoff = $('<div/>', { id: 'vonoff' }).appendTo(grid).text('Check me!')
 
@@ -77973,7 +77982,297 @@ e.meditation = mid => {
   window.mm = { m1, m2 }
 }
 
-},{"../maestro.js":217,"../router.js":222,"../transfer.js":224,"../utils.js":225,"jquery":60,"pixi.js":204,"tone":212}],221:[function(require,module,exports){
+},{"../maestro.js":217,"../router.js":223,"../transfer.js":225,"../utils.js":226,"jquery":60,"pixi.js":204,"tone":212}],221:[function(require,module,exports){
+const t = require('tone')
+const $ = require('jquery')
+const PIXI = require('pixi.js')
+const maestro = require('../maestro.js')
+const transfer = require('../transfer.js')
+const utils = require('../utils.js')
+const u = require('../router.js').urlArgument
+
+const e = module.exports
+const tr = PIXI.utils.string2hex
+
+e.meditation = mid => {
+  // todo:
+  // put a field for a message, prayer or so.
+  //   templates:
+  //      Eu <nome> inicio minha mentalizacao.... Frequencias X Y... etc.
+  //      Peço a companhia de Jesus, dos mestres iluminados, de meus espíritos aliados e esíritos afins, ...(quaisquer eixos de devoção pessoal)..., para que minha fé seja fortalecida e minhas orações escutadas.
+  // a help icon:
+  //  when hover, show some guidance:
+  //    change volume of headphones
+  //    change screen luminosity
+  //    concentrate
+  //    you can close your eyes if you wish
+  //    breath with the vertical position of the circles and the expansion of the circle to the (right, left, center, check bPos index)
+  //  if clicked, show the inhale / exhale text
+  // waveforms for LR
+  // info on the settings
+  console.log(mid, 'TMID')
+  transfer.findAny({ meditation: mid }).then(s => { // s === settings
+    if (s === null) {
+      grid.css('background', 'red')
+      countdown.text("don't exist")
+      conoff.attr('disabled', true)
+      vonoff.text('-----')
+    }
+    let duration = (s.dateTime.getTime() - (new Date()).getTime()) / 1000
+    if (u('t')) {
+      duration = parseFloat(u('t'))
+    }
+    if (duration < 0) {
+      vonoff.text('Already started, maybe finished, ask team for another session.')
+      conoff.attr('checked', true).attr('disabled', true)
+      countdown.text('finished')
+      grid.css('background', '#bbaaff')
+      return
+    }
+    setCountdown(duration, fun1)
+    function fun1 () { // to start the med
+      if (!conoff.prop('checked')) {
+        grid.css('background', 'blue')
+        countdown.text('finished')
+        conoff.prop('disabled', true)
+        return
+      }
+      const { synth, synthR, mod } = setSounds(s)
+      countdown.text('started')
+      t.Master.mute = false
+      synth.volume.rampTo(-40, 1)
+      synthR.volume.rampTo(-40, 1)
+      mod.frequency.rampTo(1 / s.mp1, s.md)
+      grid.css('background', 'lightgreen')
+      setCountdown(s.d, fun2, [synth, synthR])
+    }
+    function fun2 (synth, synthR) { // to finish the med
+      grid.css('background', 'blue')
+      countdown.text('finished')
+      synth.volume.rampTo(-400, 10)
+      synthR.volume.rampTo(-400, 10)
+    }
+    grid.css('background', 'yellow')
+  })
+  function setCountdown (duration, fun, args) { // duration in seconds
+    const targetTime = (new Date()).getTime() / 1000 + duration
+    setTimeout(() => {
+      fun(...(args || []))
+      clearInterval(timer)
+    }, duration * 1000, ...(args || []))
+    const reduce = dur => [Math.floor(dur / 60), Math.floor(dur % 60)]
+    const p = num => num < 10 ? '0' + num : num
+    const timer = setInterval(() => {
+      const moment = targetTime - (new Date()).getTime() / 1000
+      let [minutes, seconds] = reduce(moment)
+      let hours = ''
+      if (minutes > 59) {
+        [hours, minutes] = reduce(minutes)
+        hours += ':'
+      }
+      countdown.text(`countdown on ${hours}${p(minutes)}:${p(seconds)}`)
+    }, 100)
+  }
+  const nodeContainer = new PIXI.ParticleContainer(10000, {
+    scale: true,
+    position: true
+  })
+
+  const myCircle = new PIXI.Graphics() // left static circle
+    .beginFill(0xffffff)
+    .drawCircle(0, 0, 5)
+    .endFill()
+  const myCircle4 = new PIXI.Graphics() // vertical for breathing
+    .beginFill(0xffffff)
+    .drawCircle(0, 0, 5)
+    .endFill()
+
+  const app = new PIXI.Application({ // todo: make it resizable
+    width: window.innerWidth,
+    height: window.innerHeight * 0.85
+  })
+  document.body.appendChild(app.view)
+  window.appp = app
+  const [w, h] = [app.view.width, app.view.height]
+
+  const circleTexture = app.renderer.generateTexture(myCircle)
+  // const circleTexture = PIXI.Texture.from('assets/heart.png') // todo: integrate images: chokurei, sei-he-ki, heart, jesus, dove, star of david
+  app.stage.addChild(nodeContainer)
+  function mkNode (pos, scale = 1, tint = 0xffffff) {
+    const circle = new PIXI.Sprite(circleTexture)
+    circle.position.set(...pos)
+    circle.anchor.set(0.5, 0.5)
+    circle.scale.set(scale, scale)
+    circle.tint = tint
+    nodeContainer.addChild(circle)
+    return circle
+  }
+  const [x0, y0] = [w * 0.2, h * 0.2]
+  const theCircle = mkNode([x0, y0]) // moving white circle to which the flakes go
+  const myCircle_ = mkNode([x0, y0]) // fixed left
+  const myCircle__ = mkNode([x0, y0]) // fixed right
+  const myCircle2 = mkNode([x0, y0], 1, 0xffff00) // lateral
+  const myCircle3 = mkNode([x0, y0], 1, 0x00ff00) // center
+
+  // to draw the sinusoid:
+  const myLine = new PIXI.Graphics()
+  const [x, y] = [w * 0.1, h * 0.5]
+  const [dx, dy] = [w * 0.8, h * 0.4]
+  myLine.lineStyle(1, 0xffffff)
+    .moveTo(x, y)
+  const segments = 100
+  for (let i = 0; i <= segments; i++) {
+    myLine.lineTo(x + dx * i / segments, y + Math.sin(2 * Math.PI * i / segments) * dy)
+  }
+
+  const c = new PIXI.Container()
+  app.stage.addChild(c)
+  c.addChild(myLine)
+  c.addChild(myCircle)
+  c.addChild(myCircle4)
+  myCircle.position.set(-x, -y)
+  myCircle__.position.set(x, y)
+  myCircle_.position.set(x + dx, y)
+
+  function setSounds (s) {
+    theCircle.tint = myCircle_.tint = myCircle__.tint = myLine.tint = tr(s.fgc)
+    myCircle2.tint = tr(s.lcc)
+    myCircle3.tint = tr(s.ccc)
+    myCircle4.tint = tr(s.bcc)
+    myCircle4.x = s.bPos === 0 ? x + dx / 2 : s.bPos === 1 ? x * 0.5 : x + dx * 1.05
+    console.log(s, 'SET')
+    app.renderer.backgroundColor = tr(s.bgc)
+    const synth = maestro.mkOsc(0, -400, -1, 'sine')
+    const synthR = maestro.mkOsc(0, -400, 1, 'sine')
+    const mul = new t.Multiply(s.ma)
+    const mod_ = maestro.mkOsc(1 / s.mp0, 0, 0, 'sine', true).connect(mul)
+    const addL = new t.Add(s.fl)
+    const addR = new t.Add(s.fr)
+    mul.connect(addL)
+    mul.connect(addR)
+    addL.connect(synth.frequency)
+    addR.connect(synthR.frequency)
+    window.sss = { synth, synthR }
+
+    const met2 = new t.DCMeter()
+    mod_.connect(met2)
+
+    let propx = 1
+    let propy = 1
+    let rot = Math.random() * 0.1
+    const parts = []
+    window.pparts = parts
+    let f1 = (n, sx, sy, mag) => {
+      n.x += sx / mag + (Math.random() - 0.5) * 5
+      n.y += sy / mag + (Math.random() - 0.5) * 5
+    }
+    if (s.rainbowFlakes) {
+      f1 = (n, sx, sy, mag) => {
+        n.x += sx / mag + (Math.random() - 0.5) * 5
+        n.y += sy / mag + (Math.random() - 0.5) * 5
+        n.tint = (n.tint + 0xffffff * 0.1 * Math.random()) % 0xffffff
+      }
+    }
+    let lastdc = 0
+    const neg = new t.Negate()
+    const mul1 = new t.Multiply(1)
+    mod_.fan(neg, mul1)
+    mul1.connect(synth.panner.pan) // dc
+    neg.connect(synthR.panner.pan) // -dc
+    app.ticker.add(() => {
+      const dc = met2.getValue()
+      // const intensity = (1 - Math.abs(dc)) * 255
+      if (dc - lastdc > 0) { // inhale
+        // mais proximo de 1, mais azul
+        // m1.css('background', `rgba(${intensity}, ${intensity}, 0, ${1 - Math.abs(dc)})`)
+        m1.css('opacity', 1 - Math.abs(dc))
+        m2.css('opacity', 0)
+      } else { // exhale
+        // mais proximo de -1, mais azul
+        m2.css('opacity', 1 - Math.abs(dc))
+        m1.css('opacity', 0)
+        // m2.css('background', `rgba(${intensity}, ${intensity}, 0, ${1 - Math.abs(dc)})`)
+      }
+      lastdc = dc
+      // m1.text(met.getValue().toFixed(3))
+      // m2.text(dc.toFixed(3))
+      const val = -dc
+      const avalr = Math.asin(val)
+      const px = (avalr < 0 ? 2 * Math.PI + avalr : avalr) / (2 * Math.PI) * dx + x
+      const px2 = (Math.PI - avalr) / (2 * Math.PI) * dx + x
+
+      myCircle2.x = px
+      myCircle2.y = myCircle3.y = myCircle4.y = val * dy + y
+      myCircle3.x = px2
+
+      const sc = 0.3 + (-val + 1) * 3
+      myCircle4.scale.set(sc * propx, sc * propy)
+      myCircle4.rotation += rot
+
+      if (s.ellipse && sc - 0.3 < 0.0005) {
+        rot = Math.random() * 0.1
+        propx = Math.random() * 0.6 + 0.4
+        propy = 1 / propx
+      }
+
+      parts.push(mkNode([myCircle2.x, myCircle2.y], 0.3, myCircle2.tint))
+      parts.push(mkNode([myCircle3.x, myCircle3.y], 0.3, myCircle3.tint))
+      if (Math.random() > 0.98) {
+        parts.push(mkNode([myCircle4.x, myCircle4.y], 0.3, myCircle4.tint))
+      }
+
+      theCircle.x += (Math.random() - 0.5)
+      theCircle.y += (Math.random() - 0.5)
+      for (let ii = 0; ii < parts.length; ii++) {
+        const n = parts[ii]
+        const sx = theCircle.x - n.x
+        const sy = theCircle.y - n.y
+        const mag = (sx ** 2 + sy ** 2) ** 0.5
+        if (mag < 5) {
+          parts.splice(ii, 1)
+          n.destroy()
+        } else {
+          f1(n, sx, sy, mag)
+        }
+      }
+    })
+    // }, 10)
+    return { synth, synthR, mod_ }
+  }
+
+  const grid = utils.mkGrid(2)
+  $('<div/>').appendTo(grid).text('status:')
+  const countdown = $('<div/>', { id: 'countdown' }).appendTo(grid)
+  grid.css('background', 'grey')
+
+  const vonoff = $('<div/>', { id: 'vonoff' }).appendTo(grid).text('Check me!')
+
+  t.Master.mute = true
+  window.ttt = t
+  const conoff = $('<input/>', {
+    type: 'checkbox'
+  }).appendTo(grid).change(function () {
+    if (this.checked) {
+      this.disabled = true
+      t.start()
+      t.Master.mute = true
+      vonoff.text('All set!')
+      grid.css('background', 'green')
+    }
+  })
+
+  $('<div/>').text('inhale').appendTo(grid)
+  const m1 = $('<div/>', { id: 'meter1' }).appendTo(grid)
+    .css('background', 'rgb(255,255,0)')
+    .css('opacity', 0)
+  $('<div/>').text('exhale').appendTo(grid)
+  const m2 = $('<div/>', { id: 'meter2' }).appendTo(grid)
+    .css('background', 'rgb(255,255,0)')
+    .css('opacity', 0)
+  window.mm = { m1, m2 }
+}
+
+},{"../maestro.js":217,"../router.js":223,"../transfer.js":225,"../utils.js":226,"jquery":60,"pixi.js":204,"tone":212}],222:[function(require,module,exports){
 const Graph = require('graphology')
 const { erdosRenyi } = require('graphology-generators/random')
 const PIXI = require('pixi.js')
@@ -78050,7 +78349,7 @@ e.ParticleNet = class {
   }
 }
 
-},{"graphology":55,"graphology-generators/random":47,"pixi.js":204}],222:[function(require,module,exports){
+},{"graphology":55,"graphology-generators/random":47,"pixi.js":204}],223:[function(require,module,exports){
 /* global wand */
 const e = module.exports
 
@@ -78066,7 +78365,7 @@ e.urlArgument = (arg, rotOrFun) => {
   }
 }
 
-},{}],223:[function(require,module,exports){
+},{}],224:[function(require,module,exports){
 const PIXI = require('pixi.js')
 const forceAtlas2 = require('graphology-layout-forceatlas2')
 
@@ -79360,7 +79659,7 @@ e.communion = () => {
 
   <p>The outline is not rigid and intended as follows:
   <ul>
-    <li>10 minutes to gather, talk, and agree on the mentalization subject.</li>
+    <li>10 minutes to gather, talk, agree on the mentalization subject and preparations in general.</li>
     <li>15 minutes of meditation, with breathing and brainwaves synchronized through the online gadgets linked below. Thus <b>anyone that arrives late looses the meditation, there is no way around it</b>.</li>
     <li>5 minutes for final words and considerations and farewells.</li>
   </ul>
@@ -79369,7 +79668,7 @@ e.communion = () => {
   `)
   const l = t => `<a href="?m=${t}" target="_blank">${t}</a>`
   const grid = utils.mkGrid(2)
-  $('<span/>').html('<b>when</b> (UTC)').appendTo(grid)
+  $('<span/>').html('<b>when</b> (GMT-0)').appendTo(grid)
   $('<span/>').html('<b>subject</b>').appendTo(grid)
   transfer.findAll({ communionSchedule: true }).then(r => {
     window.myr = r
@@ -79387,7 +79686,98 @@ e.communion = () => {
   })
 }
 
-},{"./maestro.js":217,"./med":218,"./net.js":221,"./router.js":222,"./transfer.js":224,"./utils.js":225,"@eastdesire/jscolor":1,"flatpickr":43,"graphology-layout-forceatlas2":50,"jquery":60,"pixi.js":204,"tone":212}],224:[function(require,module,exports){
+e.panTest2 = () => {
+  const synth = maestro.mkOsc(0, -400, -1, 'sine')
+  const synthR = maestro.mkOsc(0, -400, -1, 'sine')
+  const mul = new t.Multiply(20)
+  const mod_ = maestro.mkOsc(0.1, 0, 0, 'sine', true).connect(mul)
+  const addL = new t.Add(700)
+  const addR = new t.Add(200)
+
+  mul.connect(addL)
+  mul.connect(addR)
+  addL.connect(synth.frequency)
+  addR.connect(synthR.frequency)
+
+  const neg = new t.Negate()
+  const mul1 = new t.Multiply(1)
+  mod_.connect(neg)
+  mod_.connect(mul1)
+  mul1.connect(synth.panner.pan) // dc
+  neg.connect(synthR.panner.pan) // -dc
+
+  const met2 = new t.DCMeter()
+  const me = new t.DCMeter()
+  const meN = new t.DCMeter()
+  mod_.connect(met2)
+  neg.connect(meN)
+  setInterval(() => {
+    console.log([met2, me, meN].map(i => i.getValue()))
+    console.log(synth.panner.pan.value, synthR.panner.pan.value)
+  }, 500)
+  const grid = utils.mkGrid(2)
+  const vonoff = $('<div/>', { id: 'vonoff' }).appendTo(grid).text('Stopped')
+  $('<input/>', {
+    type: 'checkbox'
+  }).appendTo(grid).change(function () {
+    if (this.checked) {
+      // t.context.resume()
+      t.start()
+      synth.volume.rampTo(-20, 1)
+      synthR.volume.rampTo(-20, 1)
+      t.Master.mute = false
+      vonoff.text('Playing')
+    } else {
+      vonoff.text('Stopped')
+    }
+  })
+  window.maux = { mod_, me, meN, neg, synth, synthR }
+}
+
+e.panBug = () => { // todo: post on tonejs' github
+  const synth = maestro.mkOsc(0, -400, -1, 'sine')
+  const synth2 = maestro.mkOsc(0, -400, -1, 'sine')
+  const synth3 = maestro.mkOsc(0, -400, -1, 'sine')
+  const mul = new t.Multiply(20)
+  const mod_ = maestro.mkOsc(0.1, 0, 0, 'sine', true).connect(mul)
+  const addL = new t.Add(700)
+  const addR = new t.Add(200)
+
+  mul.connect(addL)
+  mul.connect(addR)
+  addL.connect(synth.frequency)
+  addR.connect(synth2.frequency)
+
+  const neg = new t.Negate()
+  const mul1 = new t.Multiply(1)
+  mod_.connect(neg)
+  mod_.connect(mul1)
+  mul1.connect(synth.panner.pan)
+  neg.connect(synth2.panner.pan)
+  mod_.connect(synth3.panner.pan) // this should entail the same result as synth + mul1, no?
+
+  setInterval(() => {
+    console.log(synth.panner.pan.value, synth2.panner.pan.value, synth3.panner.pan.value)
+  }, 500)
+  const grid = utils.mkGrid(2)
+  const vonoff = $('<div/>', { id: 'vonoff' }).appendTo(grid).text('Stopped')
+  $('<input/>', {
+    type: 'checkbox'
+  }).appendTo(grid).change(function () {
+    if (this.checked) {
+      t.start()
+      synth.volume.rampTo(-20, 1)
+      synth2.volume.rampTo(-20, 1)
+      synth3.volume.rampTo(-20, 1)
+      vonoff.text('Playing')
+    } else {
+      vonoff.text('Stopped')
+    }
+  })
+  window.maux = { mod_, neg, synth, synth2, synth3 }
+}
+
+},{"./maestro.js":217,"./med":218,"./net.js":222,"./router.js":223,"./transfer.js":225,"./utils.js":226,"@eastdesire/jscolor":1,"flatpickr":43,"graphology-layout-forceatlas2":50,"jquery":60,"pixi.js":204,"tone":212}],225:[function(require,module,exports){
 const s = require('mongodb-stitch-browser-sdk')
 const e = module.exports
 
@@ -79442,7 +79832,7 @@ e.remove = query => {
   })
 }
 
-},{"mongodb-stitch-browser-sdk":106}],225:[function(require,module,exports){
+},{"mongodb-stitch-browser-sdk":106}],226:[function(require,module,exports){
 const e = module.exports
 const $ = require('jquery')
 
